dafd82154ca5f85334ecf8396d39d5842a828adf
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-03-18 22:04:48 +0530

    Add files via upload

---
+++ Shapes.py
@@ -0,0 +1,18 @@
+print("|\     |")
+print("| \    |")
+print("|  \   |")
+print("|   \  |")
+print("|    \ |")
+print("|     \|")
+
+print("------------------------------------------")
+
+print("*" * 10)
+
+print(''' This is Niishant, 
+    I am here
+    to say ro hd
+    dk
+     dfldf           kffkf l             flf fl
+     
+''')
