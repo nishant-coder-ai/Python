e7dc52d337328742ecff6b8a25c1010139da2d27
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-03-18 22:05:04 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,6 @@
+a = int((input("Enter the Number : ")))
+print(f"You have Entered : {a} ")
+
+name = input("What is Your Name : ")
+favourite_lang = input("What is Your Favorite Coding Langauge : ")
+print("Hi , World  " + name + " likes " + favourite_lang + " for Codinng :) ")
