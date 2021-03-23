cdfa3fb74b688cc5e1969e5946e54e308ff99902
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-03-23 13:58:02 +0530

    Add files via upload

---
+++ Arithematic_Operators.py
@@ -0,0 +1,23 @@
+print(1)
+print(1 + 6)
+print(6 * 5)
+print(6 / 4)      # This Will Give a Floating Point Number
+print(6 // 4)     # This will Give an integer
+print(10 % 3)
+# ** means Power
+print(6 ** 2)   # 6 square
+print(6 ** 3)   # 6 cube
+
+x = 56
+x += 3             # x = x + 3
+print(x)
+x -= 3
+print(x)
+
+n = 3.7
+print(round(n))         # if number >= 0.5 then increment by 1
+print(abs(-78.99))      # always return Positive number
+
+import math
+print(math.ceil(2.4))    # always return number after that point
+print(math.floor(2.4))    # always return number before that point
---
+++ BirthDate.py
@@ -0,0 +1,5 @@
+birth_year = input("Enter Your Birth Year : ")
+age = 2021 - int(birth_year)
+print(age)
+print(type(birth_year))
+print(type(age))
