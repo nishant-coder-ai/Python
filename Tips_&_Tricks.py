2dc63de8feb7a972d66337681a23cf0ba46eab75
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-03-28 20:43:25 +0530

    Add files via upload

---
+++ Tips_&_Tricks.py
@@ -0,0 +1,15 @@
+condition = True
+#if condition:
+#    print("1")
+#else:
+#    print("0")
+# above code can be written in one line -->
+print("1") if condition else print("0")
+
+print('////////////////////////////////////////////////////////////////')
+
+# we can put _ insted of , in large numbers;
+a = 10_00_000       # it doesn't affect the number.
+b = 999
+print(f'{a+b :,}')     # this will print ,,, in number.
+print('////////////////////////////////////////////////////////////////')
