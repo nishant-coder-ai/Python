0d18260656f5f3d55dfcdd5ae6679c1fd1f4b1c5
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-04-06 08:48:41 +0530

    Add files via upload

---
+++ Modules.py
@@ -0,0 +1,9 @@
+import Tools_Modules
+
+print(Tools_Modules.roll_dice(5))
+print(Tools_Modules.feet_in_mile)
+print(Tools_Modules.get_file_ext("Brooklyn.py"))
+print(Tools_Modules.meter_in_kilometer)
+
+
+# go and Search on google about python modules
---
+++ import_class.py
@@ -0,0 +1,4 @@
+from Class import Mobile   # from the Class file import Mobile Class
+# make object of Mobile Class
+mobile1 = Mobile()
+print(mobile1)
