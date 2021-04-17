a2e0730750e9d5131abfa4c25aac06f40f5fa556
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-04-17 08:22:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+# import ecommerce.shipping
+# ecommerce.shipping.calc_shipping()
+# or we can assign in different way
+# from ecommerce import shipping                --> in this shipping is a object
+# shipping.calc_shipping
+from ecommerce.shipping import calc_shipping
+calc_shipping()
