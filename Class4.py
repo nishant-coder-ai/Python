dc7979577dabe013f31b827ba5cdb5f73db6c814
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-04-17 08:22:12 +0530

    Add files via upload

---
+++ Class4.py
@@ -0,0 +1,8 @@
+class Math:
+    def square(self, x):
+        return x*x
+
+
+m1 = Math()
+square = m1.square(8)
+print(square)
---
+++ Class5.py
@@ -0,0 +1,7 @@
+class Math:
+    def __init__(self, x, y):    # This is a Constructor.
+        print(x*y)
+        print('This is a Constructor which is called when the object is created.')
+
+
+m1 = Math(3, 5)
