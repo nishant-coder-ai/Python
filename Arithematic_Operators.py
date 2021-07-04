2d7b44589055adf218c23c1e2d1c4c543f68c3b9
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-07-04 17:40:05 +0530

    Update Arithematic_Operators.py

--- Arithematic_Operators.py
+++ Arithematic_Operators.py
@@ -21,3 +21,5 @@ print(abs(-78.99))      # always return Positive number
 import math
 print(math.ceil(2.4))    # always return number after that point
 print(math.floor(2.4))    # always return number before that point
+
+print("This is the first time , i am Commiting ")
