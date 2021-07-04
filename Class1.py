40df19b8764b374c8e06c0712175f9b2c80cf52b
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-07-04 17:47:53 +0530

    Update Class1.py

--- Class1.py
+++ Class1.py
@@ -1,5 +1,10 @@
 class Mobile:
-    pass
+    def __int__(self, ram, processor, price, rom, company):
+        self.ram = ram
+        self.processor = processor
+        self.price = price
+        self.rom = rom
+        self.company = company
 
 
 m1 = Mobile
